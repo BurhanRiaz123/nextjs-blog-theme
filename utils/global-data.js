bf15770273ef3fdc6e8f8a0c4bc9484de9bf1f1d
Burhan Bajwa <58254452+BurhanRiaz123@users.noreply.github.com>
2024-06-29 18:00:54 +0500

    Update global-data.js

--- utils/global-data.js
+++ utils/global-data.js
@@ -2,9 +2,7 @@ export const getGlobalData = () => {
   const name = process.env.BLOG_NAME
     ? decodeURI(process.env.BLOG_NAME)
     : 'Jay Doe';
-  const blogTitle = process.env.BLOG_TITLE
-    ? decodeURI(process.env.BLOG_TITLE)
-    : 'Next.js Blog Theme';
+  const blogTitle = 'Get to Know Your Favourite Smartphone';
   const footerText = process.env.BLOG_FOOTER_TEXT
     ? decodeURI(process.env.BLOG_FOOTER_TEXT)
     : 'All rights reserved.';
